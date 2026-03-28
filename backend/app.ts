06572e8f8748b6368b0fa955f141317359acedc3
OnyemaAnthony <Anthonychibuike246@gmail.com>
2026-03-28 23:13:13 +0100

    hide stacktrace on prod

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import userRouter from './routes/users.js';
+import { globalErrorHandler } from './middleware/errorHandler.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRouter);
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
