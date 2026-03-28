f70e40899abbef090b46d6ea6f52ab7bc9e6650a
OnyemaAnthony <Anthonychibuike246@gmail.com>
2026-03-29 00:15:42 +0100

    implement order endpoint with pagination

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRouter from './routes/users.js';
+import ordersRouter from './routes/orders.js';
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRouter);
+app.use('/orders', ordersRouter);
 
 export default app;
